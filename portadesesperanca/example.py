07f2dbc7e92cdc0c9480edefb3e72fb484e6b953
Barrabin Fc. ⚑ <barrabin.fc@gmail.com>
2015-03-11 13:34:31 -0300

    Testing pyomxplayerrr

--- portadesesperanca/example.py
+++ portadesesperanca/example.py
@@ -3,6 +3,10 @@ from pprint import pprint
 
 import RPi.GPIO as GPIO
 
+import time
+usleep = lambda x: time.sleep(x/1000000.0)
+
+
 # Control a LED
 #pins = {'door': 11,
 #        'led': 18}
@@ -12,14 +16,24 @@ import RPi.GPIO as GPIO
 # Read a button
 #GPIO.setup( pins['door'], GPIO.IN)
 
-omx = OMXPlayer('/home/pi/earth.mp4')
+omx = OMXPlayer('/home/pi/raspberry-test.mp4', '-b -o hdmi --loop')
 pprint(omx.__dict__)
 
 # Door state tracking
 #door_state = 0
 #playing = False
+omg.toggle_pause()
+usleep( 100 )
 
-#while 1:
+while 1:
+    print omx.position
+    usleep( 100 )
+
+    if omx.position <= 0.00:
+        omx.toggle_pause()
+        omx.position = 0.0
+        omx.toggle_pause()
+
 #    door_state = GPIO.input( pins['door'] )
 #    if door_state == 1:             # OPEN
 #        if not playing:
@@ -29,7 +43,3 @@ pprint(omx.__dict__)
 #            playing = True
 #    else:                           # Closed
 #        omx.pause()
-#        playing = False
-
-omx.toggle_pause()
-omx.stop()
